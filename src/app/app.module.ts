0bae39f109e32b49f2b517d6c74473752a8c200c
aqeel parpia <aqeelparpia786@hotmail.com>
2020-01-18 14:25:38 -0500

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ProtfolioComponent } from './protfolio/protfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    GalleryComponent,
+    ProtfolioComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
