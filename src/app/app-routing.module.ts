c1880fbbb3f4856999dbcb54d15ffed8478aa61f
aqeel parpia <aqeelparpia786@hotmail.com>
2020-01-30 00:07:09 -0500

    updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ProtfolioComponent } from './protfolio/protfolio.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { VideosComponent } from './videos/videos.component';
 
 const routes: Routes = [
     { path: 'home', component: HomeComponent},
-    { path: 'protfolio',component: ProtfolioComponent},
+    { path: 'gallery',component: GalleryComponent},
+    { path: 'videos',component: VideosComponent},
     { path: 'about', component: AboutComponent},
     { path: 'contact', component: ContactComponent},
 ];
@@ -17,5 +19,5 @@ const routes: Routes = [
     exports: [RouterModule]
   })
   export class AppRoutingModule { }
-  export const routingComponents = [HomeComponent,AboutComponent,ProtfolioComponent,ContactComponent]
+  export const routingComponents = [HomeComponent,AboutComponent,GalleryComponent,VideosComponent,ContactComponent]
   
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
+import { VideosComponent } from './videos/videos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    routingComponents
+    routingComponents,
+    VideosComponent
   ],
   imports: [
     BrowserModule,
